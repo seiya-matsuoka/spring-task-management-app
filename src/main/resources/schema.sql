1580abfed98f13ef9326be163975eafcf5a4e11d
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2025-08-15 14:47:52 +0900

    feat: 初期テーブルの内容を修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 create table tasks (
-id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-summary VARCHAR(256) NOT NULl,
-description VARCHAR(256) NOT NULl
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    summary VARCHAR(256) NOT NULl,
+    description VARCHAR(256) NOT NULl,
+    priority INT NOT NULl,
+    created_at timestamp without time zone
 );
